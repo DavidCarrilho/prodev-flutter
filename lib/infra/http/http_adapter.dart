ae9cd97d58d4d5de46933eaa2442f7946d9b323f
DavidCarrilho <luan.david.oliver@gmail.com>
2021-09-15 23:51:47 -0300

    move HttpAdapter to its own filte

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody != null ? jsonEncode(body) : null);
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,29 +1,10 @@
-import 'dart:convert';
 
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
-class HttpAdapter {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(url, headers: headers, body: jsonBody != null ? jsonEncode(body) : null);
-  }
-}
+import 'package:appprodev/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
 
